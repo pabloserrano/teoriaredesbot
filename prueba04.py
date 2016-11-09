5d8550e9f2409c161c52ec3095f507eb2d382d3e
pabloserrano <pablo.serrano@gmail.com>
2016-11-09 11:09:22 +0100

    Update prueba04.py

--- prueba04.py
+++ prueba04.py
@@ -5,37 +5,60 @@ import time
 import datetime
 import random
 import telepot 
+import logging
 import ConfigParser
 from pushover import Client
 
+start_done = {}
+news_update = {}
+unique_users = set()
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='activity.log',
+                    filemode='a')
+
 
 def handle(msg):
-	global chat_id, news_update
+	global news_update
 	chat_id = msg['chat']['id']
 	command = msg['text']
+	key = str(chat_id)
 
 	print 'Got command: %s from %i' %(command,chat_id)
 
 	if command == '/start':
-		if chat_id == -1:
-			bot.sendMessage(chat_id, 'Bot arrancado, te tengo')
-			client.send_message("Nueva conexión al bot", title="Telegram bot")
-		else: 
-			bot.sendMessage(chat_id, 'Para qué le das a start otra vez')
+		logging.info('Comando /start de %s', chat_id)
+		if key in start_done:
+			bot.sendMessage(chat_id, 'Ya tenías hecho el start')
+		else:
+			bot.sendMessage(chat_id, 'Bot inicializado, noticias OFF por defecto')
+			start_done[key] = 1
+			news_update[key] = 0
+			unique_users.add(chat_id)
+			#client.send_message("Nueva conexión al bot", title="Telegram bot")
 	elif command == '/help':
+		logging.info('Comando /help de %s', chat_id)
 		bot.sendMessage(chat_id, 'Aqui vendría la ayuda')
 	elif command == '/settings':
-		bot.sendMessage(chat_id, 'Configuracion actual')
+		logging.info('Comando /settings de %s', chat_id)
+		bot.sendMessage(chat_id, 'Configuracion actual: News %i' %news_update[key])
 	elif command == '/newsOn':
+		logging.info('Comando /newsOn de %s', chat_id)
 		bot.sendMessage(chat_id, 'Noticias activadas')
-		news_update = 1
+		news_update[key] = 1
 	elif command == '/newsOff':
+		logging.info('Comando /newsOff de %s', chat_id)
 		bot.sendMessage(chat_id, 'Noticias desactivadas')
-		news_update = 0
+		news_update[key] = 0
+	elif command == '/stats':
+		logging.info('Comando /stats de %s', chat_id)
+		bot.sendMessage(chat_id, 'En total he visto %i IDs' % len(unique_users))
+		
 
 
-chat_id = -1
-news_update = 0
+
+
 
 config = ConfigParser.ConfigParser()
 config.readfp(open(r'config.txt'))
@@ -45,21 +68,22 @@ PUSHOVER_API = config.get('Tokens', 'pushover_api')
 PUSHOVER_CLIENT = config.get('Tokens', 'pushover_client')
 
 client = Client(PUSHOVER_CLIENT, api_token=PUSHOVER_API)
+
 bot = telepot.Bot(TELEGRAM_TOKEN)
 
 bot.message_loop(handle)
 print('Listening ...')
+logging.info('Arranco el bot')
 
-client.send_message("Bot de teoría de redes arrancado", title="Telegram bot")
+
+#client.send_message("Bot de teoría de redes arrancado", title="Telegram bot")
 
 while 1: 
 	time.sleep(10)
-	if news_update == 1: 
-		if chat_id == -1:
-			print('Aquí hubo un error, no tengo chat id')
-		else:
-			bot.sendMessage(chat_id, 'Noticias de las')
-			bot.sendMessage(chat_id, str(datetime.datetime.now()))
+	for x in news_update:
+		if news_update[x] == 1: 
+			bot.sendMessage(x, 'Noticias de las')
+			bot.sendMessage(x, str(datetime.datetime.now()))
 
 
 
