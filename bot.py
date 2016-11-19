d36a2136b81a4f17a4d071f884b41a0a539c2900
pabloserrano <pablo.serrano@gmail.com>
2016-11-19 17:26:04 +0100

    Estadisticas y mas encuestas

--- bot.py
+++ bot.py
@@ -49,6 +49,7 @@ def admin(bot, update):
 
 def alert(bot, update, args):
 	global users
+	logging.info('Usuario %i comando /alert ' % update.message.chat_id)
 	if int(users[str(update.message.chat_id)][0]):
 		chat_id = update.message.chat_id
 		if len(args) == 0:
@@ -67,27 +68,30 @@ def start(bot, update):
 	global users
 	global FILE_USERS
 	if str(update.message.chat_id) not in users:
-		update.message.reply_text('Hola!')
+		update.message.reply_text('Hola! Por defecto, noticias y encuestas activadas (comando /settings para cambiar configuracion)')
 		users[str(update.message.chat_id)].append(0)
-		users[str(update.message.chat_id)].append(0)
-		users[str(update.message.chat_id)].append(0)
-		logging.info('/start nuevo usuario: %i usuarios' % update.message.chat_id)
+		users[str(update.message.chat_id)].append(1)
+		users[str(update.message.chat_id)].append(1)
+		logging.info('/start nuevo usuario: %i ' % update.message.chat_id)
 		save_config_users(FILE_USERS, users)
 	else:
 		update.message.reply_text('Hola de nuevo')
-		logging.info('/start usuario %i vuelve' % update.message.chat_id)
+		logging.info('/start vuelve usuario %i ' % update.message.chat_id)
 	
 
 
 def help(bot, update):
+	logging.info('Usuario %i comando /help ' % update.message.chat_id)
 	if int(users[str(update.message.chat_id)][0]):
-		update.message.reply_text('/alert para enviar una alerta, /settings para configurar los parámetros, /freetext para enviar texto libre, /randomfact para una curiosidad al azar')
+		update.message.reply_text('/alert para enviar una alerta, /settings para configurar los parámetros, /freetext para enviar texto libre, /randomfact para una curiosidad al azar, /stats para estadisticas')
 	else:
 		update.message.reply_text('/settings para configurar los parámetros, /freetext para enviar texto libre, /randomfact para una curiosidad al azar')
 
 
 def randomfact(bot, update):
+	global users
 	global FILE_RANDOM
+	logging.info('Usuario %i comando /randomfact ' % update.message.chat_id)
 
 	lines = open(FILE_RANDOM).read().splitlines()
 	myline = random.choice(lines)
@@ -97,29 +101,51 @@ def randomfact(bot, update):
 	keyboard = [[InlineKeyboardButton("Si", callback_data='r.'+identifier+'.si'),
 		InlineKeyboardButton("No", callback_data='r.'+identifier+'.no')]]
 	reply_markup = InlineKeyboardMarkup(keyboard)
-	update.message.reply_text('Te ha parecido interesante?', reply_markup=reply_markup)
+	if int(users[str(update.message.chat_id)][1]):
+		update.message.reply_text('Te ha parecido interesante?', reply_markup=reply_markup)
 
 
 def settings(bot, update):
 	global users
 	global FILE_USERS
-	#update.message.reply_text('Cambiar configuración:')
+	logging.info('Usuario %i comando /settings ' % update.message.chat_id)
 
 	keyboard = [[InlineKeyboardButton("Activar", callback_data='s.news.si'),
 		InlineKeyboardButton("Desactivar", callback_data='s.news.no')]]
 	reply_markup = InlineKeyboardMarkup(keyboard)
-	update.message.reply_text('Recibir avisos y noticias', reply_markup=reply_markup)
+	update.message.reply_text('Recibir noticias', reply_markup=reply_markup)
 
 	keyboard = [[InlineKeyboardButton("Activar", callback_data='s.polls.si'),
 		InlineKeyboardButton("Desactivar", callback_data='s.polls.no')]]
 	reply_markup = InlineKeyboardMarkup(keyboard)
 	update.message.reply_text('Participar en encuestas', reply_markup=reply_markup)
 
+def activity_count(datestamp, fname):
+	keyword = datestamp.strftime("%Y-%m-%d")
+	with open(fname, 'r') as fin:
+		return sum([1 for line in fin if keyword in line])
+
+def stats(bot, update):
+	global users
+	global FILE_USERS
+	global FILE_LOG
+	logging.info('Usuario %i comando /stats ' % update.message.chat_id)
+	if int(users[str(update.message.chat_id)][0]):
+		num_users = sum(1 for line in open(FILE_USERS)) - 1
+		update.message.reply_text('Hay %i usuarios' % num_users)
+		today = datetime.date.today()
+		update.message.reply_text('Dia: %s actividad: %i' % (today.strftime("%Y-%m-%d"), activity_count(today,FILE_LOG)))
+		for d in xrange(1,3):
+			today = today - datetime.timedelta(days=1)
+			update.message.reply_text('Dia: %s actividad: %i' % (today.strftime("%Y-%m-%d"), activity_count(today,FILE_LOG)))
+	else:
+		update.message.reply_text('Comando no reconocido')
 
 
 def freetext(bot, update, args):
 	#TODO HAY QUE FORMATEAR LA ENTRADA QUE NO SEAN TILDES O COSAS RARAS
     global FILE_TEXT
+    logging.info('Usuario %i comando /freetext ' % update.message.chat_id)
     chat_id = update.message.chat_id
     if len(args) == 0:
     	update.message.reply_text('Uso: /freetext <texto>')
@@ -134,6 +160,7 @@ def button(bot, update):
     global users
     global FILE_USERS
     global FILE_RANDOM_RESULTS
+    global FILE_NEWS_RESULTS
 	
     query = update.callback_query
     chat_id=query.message.chat_id
@@ -160,6 +187,7 @@ def button(bot, update):
     		text="Parametro de settings no reconocido"
     	bot.editMessageText(text=text, chat_id=chat_id, message_id=message_id)
     	save_config_users( FILE_USERS, users )
+    	logging.info('Usuario %i pone configuracion polls: %s news: %s' % (chat_id, users[str(chat_id)][1], users[str(chat_id)][2]))
 
     #r para los random facts
     elif query.data[0] == 'r':
@@ -171,7 +199,19 @@ def button(bot, update):
     	with open(FILE_RANDOM_RESULTS, "a") as myfile:
     		myfile.write("%s \t %s \t %s \t %s\n" % (str(datetime.datetime.now()), identifier, str(chat_id), reply))    	
     	bot.editMessageText(text=text, chat_id=chat_id, message_id=message_id)
+    	logging.info('Usuario %i responde random %s con %s' % (chat_id, identifier, reply))
     
+    #n para las noticias
+    elif query.data[0] == 'n':
+    	dummy, identifier, reply = query.data.split(".")
+    	if reply == 'si':
+    		text="Vale!"
+    	else:
+    		text="Vaya..."
+    	with open(FILE_NEWS_RESULTS, "a") as myfile:
+    		myfile.write("%s \t %s \t %s \t %s\n" % (str(datetime.datetime.now()), identifier, str(chat_id), reply))    	
+    	bot.editMessageText(text=text, chat_id=chat_id, message_id=message_id)
+    	logging.info('Usuario %i responde news %s con %s' % (chat_id, identifier, reply))
     else:
     	print ('No entiendo')
 
@@ -194,6 +234,14 @@ def todays_news(bot,job):
 			for key in users:
 				if users[key][2] == '1':
 					bot.sendMessage(chat_id=int(key), text=calendar[todays_key])
+
+					keyboard = [[InlineKeyboardButton("Si", callback_data='n.'+todays_key+'.si'),
+					InlineKeyboardButton("No", callback_data='n.'+todays_key+'.no')]]
+					reply_markup = InlineKeyboardMarkup(keyboard)
+
+					if int(users[str(key)][1]):
+						bot.sendMessage(chat_id=int(key), text='Te ha parecido interesante?', reply_markup=reply_markup)
+
 	else:
 		logging.warn('todays_news: no hay fichero de noticias')
 
@@ -204,8 +252,11 @@ def main():
 	global FILE_USERS
 	global FILE_TEXT
 	global FILE_NEWS
+	global FILE_NEWS_RESULTS
 	global FILE_RANDOM
 	global FILE_RANDOM_RESULTS
+	global FILE_LOG
+
 
 	# Load configuration parameters
 	config = ConfigParser.ConfigParser()
@@ -213,6 +264,7 @@ def main():
 	TELEGRAM_TOKEN = config.get('Tokens', 'telegram_token')
 	FILE_USERS = config.get('Files', 'users')
 	FILE_NEWS = config.get('Files', 'news')
+	FILE_NEWS_RESULTS = config.get('Files', 'news_results')
 	FILE_TEXT = config.get('Files', 'freetext')
 	FILE_RANDOM = config.get('Files', 'randomfacts')
 	FILE_RANDOM_RESULTS = config.get('Files', 'randomfacts_results')
@@ -250,6 +302,8 @@ def main():
 	seconds = (time_news - now).total_seconds()
 	if seconds < 0:
 		seconds = seconds + 24*60*60
+	#para probar las noticias ya
+	#seconds = 3
 	jq.put(job_news, next_t=seconds)
 
 
@@ -260,6 +314,7 @@ def main():
 	dp.add_handler(CommandHandler("alert", alert, pass_args=True))
 	dp.add_handler(CommandHandler("admin", admin))
 	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(CommandHandler("stats", stats))
 	dp.add_handler(CommandHandler("help", help))
 	dp.add_handler(CommandHandler("randomfact", randomfact))
 	dp.add_handler(CommandHandler("settings", settings))
@@ -268,7 +323,7 @@ def main():
 	dp.add_handler(CallbackQueryHandler(button))
 
 	# on noncommand i.e message - echo the message on Telegram
-	dp.add_handler(MessageHandler(Filters.text, echo))
+	#dp.add_handler(MessageHandler(Filters.text, echo))
 
 	# log all errors
 	dp.add_error_handler(error)
